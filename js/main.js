843cdb31bcd28f61174daa4fab83c7afccbc1e21
DarioGuastella <jimmyguastella@gmail.com>
2023-10-31 17:02:19 +0100

    aggiunge autorizzazione con riscontro in console

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const registeredEmail = [ "pippo@gmail.com", "topolino@gmail.com",  "paperino@gmail.com", "topolina@gmail.com", "paperina@gmail.com" ];
+const btn = document.getElementById("emailBtn");
+
+btn.addEventListener("click", function() {
+    const userEmail = document.getElementById("userEmail").value;
+    console.log(userEmail);
+    if (registeredEmail.includes(userEmail,0)){
+        console.log("Sei autorizzato");
+    } else {
+        console.log("Non sei autorizzato")
+    }
+
+
+    }
+
+);
