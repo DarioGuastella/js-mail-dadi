1d29395f027da3660f0b5815761ad1a00b471eeb
DarioGuastella <jimmyguastella@gmail.com>
2023-10-31 18:50:33 +0100

    aggiunge ciclo

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-const registeredEmail = [ "pippo@gmail.com", "topolino@gmail.com",  "paperino@gmail.com", "topolina@gmail.com", "paperina@gmail.com" ];
+const registeredEmailList = [ "pippo@gmail.com", "topolino@gmail.com",  "paperino@gmail.com", "topolina@gmail.com", "paperina@gmail.com" ];
 const btn = document.getElementById("emailBtn");
+const userEmail = document.getElementById("userEmail");
+const title = document.getElementById("title");
 
 btn.addEventListener("click", function() {
-    const userEmail = document.getElementById("userEmail").value;
-    const title = document.getElementById("title");
-    if (registeredEmail.includes(userEmail,0)){
-        title.innerHTML = "Sei autorizzato";
-    } else {
-        title.innerHTML = "Non sei autorizzato";
+    
+    for (let i = 0; i < registeredEmailList.length; i++) {
+        const validEmail = registeredEmailList[i];
+        console.log( validEmail );
+        console.log(userEmail.value);
+        if (validEmail == userEmail.value){
+            title.innerHTML = "Sei autorizzato";
+            break;
+        } else {
+            title.innerHTML = "Non sei autorizzato";
+        }
     }
-});
+    });
+    
