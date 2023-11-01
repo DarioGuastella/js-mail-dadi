daa1646fdfce039e860a91128900027babc070c9
DarioGuastella <jimmyguastella@gmail.com>
2023-11-01 18:44:22 +0100

    gioco dadi

---
+++ dadi/main.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById("playBtn");
+const yourMessage = document.getElementById("yourResult");
+const pcMessage = document.getElementById("pcResult");
+btn.addEventListener("click", function() {
+    const playerNumber = Math.floor(Math.random() * 6) + 1;
+    const pcNumber = Math.floor(Math.random() * 6) + 1;
+    yourMessage.innerHTML = ( `Il tuo risultato è ${playerNumber}` );
+    pcMessage.innerHTML = ( `Il risultato dell'avversario è ${pcNumber}` );
+    if (playerNumber > pcNumber) {
+        document.getElementById("endGame").innerHTML = ("Hai vinto!");
+        document.getElementById("endGame").style.color = "green";
+    } else {
+        document.getElementById("endGame").innerHTML = ("Hai perso!");
+        document.getElementById("endGame").style.color = "red";
+    }
+});
